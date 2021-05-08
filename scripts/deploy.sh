d48e8669e468bd08d5e0a2808b3e6270a7ce5dfe
namu1714 <namu1714@naver.com>
2021-05-08 19:53:34 +0900

    chap09_배포자동화구성ver.2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,4 +34,4 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
       -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,home/ec2-user/app/application-real-db.properties,classpath:/appplication-real.properties \
       -Dspring.properties.active=real \
-      $JAR_NAME > $REPOSITORY/$JAR_NAME 2>&1 &
+      $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
